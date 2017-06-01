da2abd8a67b37d63f7f82ae881bc1efbb323d0cb
LaurentBAZILE <laurent.bazile@outlook.fr>
2017-06-01 17:17:49 +0200

    Create Nourriture.java
    Classe représentant la nourriture

---
+++ Nourriture.java
@@ -0,0 +1,16 @@
+//Classe représentant la nourriture
+
+public class Nourriture {
+    int x;
+    int y;
+    int width;
+    int height;
+    
+    public Nourriture(int fx,int fy,int fwidth,int fheight)
+    { 
+        x=fx;
+        y=fy;
+        width=fwidth;
+        height=fheight;
+    }
+}
